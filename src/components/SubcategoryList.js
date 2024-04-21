53ded58917bb7cb08ff65da4c70f27c8db2fea9a
craneklein <318885860@qq.com>
2024-04-21 16:31:26 +0800

    将二级分类从button改成span标签

--- src/components/SubcategoryList.js
+++ src/components/SubcategoryList.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // SubcategoryList组件，接受子分类数据和选择子分类的函数作为props
 function SubcategoryList({ subcategories, onSubcategorySelect }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <div className="subcategory-list">
-      {subcategories.map((sub) => (
+      {subcategories.map((sub, index) => (
         // 循环渲染每个子分类的按钮，并设置点击事件
-        <button key={sub.id} onClick={() => onSubcategorySelect(sub.id)}>
+        <span className={ currentIndex === index ? 'subActive' : ""} key={sub.id} onClick={() => {
+          onSubcategorySelect(sub.id);
+          setCurrentIndex(index);
+        }
+        }>
           {sub.name}
-        </button>
+        </span>
       ))}
     </div>
   );
